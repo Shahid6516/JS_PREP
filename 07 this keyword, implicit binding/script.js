b80cb57571dbb7fed65d40b04e2d5659d9d90554
Mohammad Shahid <www.shahid6516@gmail.com>
2025-11-04 02:02:18 +0530

    This keyword

---
+++ 07 this keyword, implicit binding/script.js
@@ -0,0 +1,23 @@
+// this keyword in javascript (implicit binding)
+// explain this keyword
+
+// this means the object that is using the function right now.
+// this keyword is use to reference something
+// In simple words:
+// 👉 “Who is calling me?”
+
+// this.a = 5;
+
+// const getPram = ()=> {
+// console.log(this.a)
+// }
+// getPram()
+
+// let user = {
+//   name: "Shahid",
+//   age: 21,
+//   getDetails() {
+//     console.log(this.name);
+//   },
+// };
+// user.getDetails();
